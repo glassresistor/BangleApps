235a15b55dec670afb2b90eb65a7c4aa5894b4cf
Mikela <glassresistor@gmail.com>
2024-04-04 20:44:09 -0700

    Update bebble.app.js

--- apps/bebble/bebble.app.js
+++ apps/bebble/bebble.app.js
@@ -52,7 +52,7 @@ function buzzForEvents() {
   let minToEvent = Math.round((nextEvent.timestamp - getTime()) / 60.0);
   //if (minToEvent <= 10) {
     g.setFontLECO1976Regular22();
-    g.setFontAlign(0, -1);
+    g.setFontAlign(0, 0);
     g.setColor(theme.day);
     g.drawString("nextEvent.title", 0, h3 + t);
   //}

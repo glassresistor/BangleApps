e6345a7cba439527a9b0aec468d000883499d3f5
Mikela <glassresistor@gmail.com>
2024-04-04 21:54:22 -0700

    Update bebble.app.js

--- apps/bebble/bebble.app.js
+++ apps/bebble/bebble.app.js
@@ -97,11 +97,15 @@ let draw = function() {
   g.setColor(theme.fg);
   g.fillRect(0, h3, w, h3 + t);
   // the bottom
-  g.setColor(settings.bg);
-  g.fillRect(0, h3 + t, w, h);
+  //g.setColor(settings.bg);
+  //g.fillRect(0, h3 + t, w, h);
   
   buzzForEvents();
-  
+  g.setFontLECO1976Regular22();
+  g.setFontAlign(0, -1);
+  g.setColor("#f00");
+  g.drawString("nextEvent.title", 0, h3+t);
+
   g.setColor(settings.bg);
   g.drawImage(img, w/2 + ((w/2) - 64)/2, 1, { scale: 1 });
   drawCalendar(((w/2) - 42)/2, 14, 42, 4, dayOfMonth);

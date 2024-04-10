7adb1b14a6e4a51e5312f7d6210e9035bf793463
Mikela Clemmons <glassresistor@gmail.com>
2024-04-09 21:05:31 -0700

    asdf

--- apps/bebble/bebble.app.js
+++ apps/bebble/bebble.app.js
@@ -24,12 +24,12 @@ let loadSettings = function() {
 };
 
 const img = require("heatshrink").decompress(atob("oFAwkEogA/AH4A/AH4A/AH4A/AE8AAAoeXoAfeDQUBmcyD7A+Dh///8QD649CiAfaHwUvD4sEHy0DDYIfEICg+Cn4fHICY+DD4nxcgojOHwgfEIAYfRCIQaDD4ZAFD5r7DH4//kAfRCIZ/GAAnwD5p9DX44fTHgYSBf4ofVDAQEBl4fFUAgfOXoQzBgIfFBAIfPP4RAEAoYAB+cRiK/SG4h/WIBAfXIA7CBAAswD55AHn6fUIBMCD65AHl4gCmcziAfQQJqfQQJpiDgk0IDXxQLRAEECaBM+QgRYRYgUIA0CD4ggSQJiDCiAKBICszAAswD55AHABKBVD7BAFABIqBD5pAFABPxD55AOD6BADiIAJQAyxLABwf/gaAPAH4A/AH4ARA=="));
-const t = 1;
+const t = 0;
 const h = g.getHeight();
 const w = g.getWidth();
-const ha = 2*h/5 - 10;
-const h2 = 3*h/5 - 16;
-const h3 = 7*h/8 - 6;
+const ha = 2*h/5 - 24;
+const h2 = 3*h/5 - 30;
+const h3 = 7*h/8 - 20;
 
 let batteryWarning = false;
 function isActive(event) {
